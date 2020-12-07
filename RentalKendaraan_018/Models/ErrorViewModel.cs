6d9c01e48ef285cb8414b284cf24b5ef4bc2f031
RamadhanFN <62781418+RamadhanFN@users.noreply.github.com>
2020-12-07 17:24:24 +0700

    Add project files.

---
+++ RentalKendaraan_018/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RentalKendaraan_018.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ RentalKendaraan_018/Models/Upload/FilesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentalKendaraan_018.Models.Upload
+{
+    public class FilesDetails
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+
+    public class FilesViewModel
+    {
+        public List<FilesDetails> Files { get; set; }
+            = new List<FilesDetails>();
+    }
+}
